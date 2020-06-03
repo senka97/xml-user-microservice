824de1242c5dca25bb0af436c9196cdd7148e071
Senka <senka.soic@uns.ac.rs>
2020-06-03 22:15:43 +0200

    Permitted path for unauthorised users.

--- src/main/java/com/team19/usermicroservice/config/SecurityConfiguration.java
+++ src/main/java/com/team19/usermicroservice/config/SecurityConfiguration.java
@@ -65,7 +65,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
                 // svim korisnicima dopusti da pristupe putanjama /auth/** i /h2-console/**
                 .authorizeRequests()
-                .antMatchers("/auth/**").permitAll()
+                .antMatchers("/auth/**", "/user/getCommentCreator").permitAll()
                 .antMatchers("/h2-console/**").permitAll()
                 .antMatchers("/ws/**").permitAll()
 
